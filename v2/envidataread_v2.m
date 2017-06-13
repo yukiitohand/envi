b7eaf2d4a64f3b135a2891ae6f3d26dc459d44a8
Yuki Itoh <yukiitoh@Yukis-MacBook-Pro.local>
2017-06-12 23:28:56 -0400

    data_ignore_value is considered in reading

--- v2/envidataread_v2.m
+++ v2/envidataread_v2.m
@@ -54,5 +54,9 @@ end
 
 img = multibandread(datafile, [lines,samples,bands],...
                     precision, header_offset, interleave, byteorder);
-img(img==hdr_info.data_ignore_value) = nan;
+
+if isfield(hdr_info,'data_ignore_value')
+    img(img==hdr_info.data_ignore_value) = nan;
+end
+
 end
