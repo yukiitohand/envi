f896a7853ec63769c9b4729dfc79f29fa089461d
Yuki Itoh <yukiitohand@gmail.com>
2017-11-16 15:50:29 -0500

    support case senstive file system

--- v2/guessEnviHDRPATH.m
+++ v2/guessEnviHDRPATH.m
@@ -5,13 +5,28 @@ function [hdrPath] = guessEnviHDRPATH(basename,dirPath)
 %   dirPath: string, directory path in which the header file is stored.
 % Output Parameters
 %   hdrPath: full file path to the header file
-
-hdrPath = joinPath(dirPath,[basename '.hdr']);
-if ~exist(hdrPath,'file')
-    hdrPath = joinPath(dirPath,[basename '.img.hdr']);
+if ismac
+    hdrPath = joinPath(dirPath,[basename '.hdr']);
     if ~exist(hdrPath,'file')
-        warning('Header file cannot be found.');
-        hdrPath = '';
+        hdrPath = joinPath(dirPath,[basename '.img.hdr']);
+        if ~exist(hdrPath,'file')
+            warning('Header file cannot be found.');
+            hdrPath = '';
+        end
+    end
+elseif isunix
+    hdrname = [basename '.hdr'];
+    [hdrname] = findfilei(hdrname,dirPath);
+    if isempty(hdrname)
+        hdrname = [basename '.img.hdr'];
+        [hdrname] = findfilei(hdrname,dirPath);
+        if isempty(hdrname)
+           warning('Header file cannot be found');
+        end
+    end
+    if ~isempty(hdrname)
+        hdrPath = joinPath(dirPath,hdrname);
     end
 end
 
+end
